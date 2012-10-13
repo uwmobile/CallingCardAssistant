62a5c8d8153514e6fe9289196d7f0fbef45eed8e
Elton Gao <gyfelton@gmail.com>
2012-10-13 13:42:23 -0400

    Initial Commit

---
+++ CallingCardAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallingCardAssistant
+//
+//  Created by Yuanfeng on 2012-10-13.
+//  Copyright (c) 2012 UW Mobile Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ CallingCardAssistant/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  CallingCardAssistant
+//
+//  Created by Yuanfeng on 2012-10-13.
+//  Copyright (c) 2012 UW Mobile Club. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
