6e24a040601ad9536b1d2eb93e08700cfee6468e
Irene <hflsjiangruowei@gmail.com>
2012-11-04 18:56:03 -0500

    fix up crash
    fix up crash when deleting nothing

--- CallingCardAssistant/DialViewController.m
+++ CallingCardAssistant/DialViewController.m
@@ -28,13 +28,10 @@
 - (IBAction)delete:(UIButton *)sender {
     NSString *string = self.display.text;
     NSInteger len = [string length];
-
-    string  = [string  substringToIndex:len-1];
-    
                
     if ([string isEqualToString:@""])
     {
-        string =@"" ;
+        string =self.display.text;
         
     }else{
         string =[string substringToIndex:len-1];
